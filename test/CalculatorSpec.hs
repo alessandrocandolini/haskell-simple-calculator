11144a39d6f3abd0c35ddd41139b4afdb6a777c4
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-11-26 11:21:42 +0000

    more tests

--- test/CalculatorSpec.hs
+++ test/CalculatorSpec.hs
@@ -10,15 +10,31 @@ example1 = add (multiply (add (value 2) (value 3)) (value 4)) (value 5)
 
 spec :: Spec
 spec = describe "Calculator" $ do
+  it "simple evaluate"
+    $ evaluate example1
+    `shouldBe` 25
 
-     it "simple evaluate" $
-        evaluate example1 `shouldBe` 25
+  it "simple pretty"
+    $ pretty example1
+    `shouldBe` "(((2+3)*4)+5)"
 
-     it "simple pretty" $
-        pretty example1 `shouldBe` "(((2+3)*4)+5)"
-
-     it "simple pretty" $ let
+  it "simplify example 1"
+    $ let
         original :: Ast
         original = add (multiply (add (value 0) (value 2)) (value 1)) (value 5)
        in
         simplify original `shouldBe` add (value 2) (value 5)
+
+  it "simplify example 2"
+    $ let
+        original :: Ast
+        original = add (multiply (add (value 1) (value 0)) (value 2)) (value 5)
+       in
+        simplify original `shouldBe` add (value 2) (value 5)
+
+  it "simplify example 3"
+    $ let
+        original :: Ast
+        original = add (multiply (add (value 1) (value 0)) (value 5)) (value 0)
+       in
+        simplify original `shouldBe` (value 5)
